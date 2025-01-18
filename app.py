872ff91d45069988a35f3d651ec3547cd52005fc
leogonzale <lgonzalez@alcapone-us.com>
2025-01-18 12:00:43 -0500

    create prompt template

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import logging
 from datetime import datetime
+from langchain_core.prompts import PromptTemplate
 
 # app will run at: http://127.0.0.1:5000/
 
@@ -11,6 +12,18 @@ log = logging.getLogger("app")
 # Initialize the Flask application
 app = Flask(__name__)
 
+	
+def build_new_trip_prompt(form_data):
+   prompt_template = PromptTemplate.from_template("This trip is to {location} between {trip_start} and {trip_end}. This person will be traveling {traveling_with_list} and would like to stay in {lodging_list}. They want to {adventure_list}. Create a daily itinerary for this trip using this information.")
+                                                  
+   return prompt_template.format(
+        location = form_data["location"],
+        trip_start = form_data["trip_start"],
+        trip_end = form_data["trip_end"],
+        traveling_with_list = form_data["traveling_with_list"],
+        lodging_list = form_data["lodging_list"],
+        adventure_list = form_data["adventure_list"]
+    )
 # Define the route for the home page
 @app.route("/", methods=["GET"])
 def index():
@@ -24,6 +37,23 @@ def plan_trip():
 # Define the route for view trip page with the generated trip itinerary
 @app.route("/view_trip", methods=["POST"])
 def view_trip():
+  #log.info(request.form)
+  traveling_with_list = ",".join(request.form.getlist("traveling-with"))
+  lodging_list = ", ".join(request.form.getlist("lodging"))
+  adventure_list = ", ".join(request.form.getlist("adventure"))
+ 
+
+  cleaned_form_data = {
+        "location": request.form["location-search"],
+        "trip_start": request.form["trip-start"],
+        "trip_end": request.form["trip-end"],
+        "traveling_with_list": traveling_with_list,
+        "lodging_list": lodging_list,
+        "adventure_list": adventure_list,
+        "trip_name": request.form["trip-name"]
+    }
+  prompt = build_new_trip_prompt(cleaned_form_data)
+  log.info(prompt)
   return render_template("view-trip.html")
     
 # Run the flask server
